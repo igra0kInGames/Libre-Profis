2af91a9285eb5d277da545007d7bfa2acea32252
Saltykov <saltykov.igra0k2010@gmail.com>
2026-07-02 19:05:48 +0300

    Добавленна библеотека pygame_gui. В classes.py остался только text. Удален functions.py из-за ненадобности. Добавлена новая страница ввода пути к файлу.

--- classes.py
+++ classes.py
@@ -1,140 +1,65 @@
 import pygame
-import logging
-import os
 
-from functions import *
-
-
-class Point:
-    def __init__(self, screen, x, y):
-        self.screen = screen
+class Button:
+    def __init__(self, x, y, scaleX, scaleY, bgColor=(255, 255, 255), text="sample text",
+                 textColor=(0, 0, 0), fontSize=16, smooth=0, borderSize=-1, screen=None):
         self.x = x
         self.y = y
-
-    def draw(self):
-        pygame.draw.circle(self.screen, (0, 0, 0), (self.x, self.y), 3)
-        logging.debug("point draw!")
-
-
-class Polygon:
-    def __init__(self, screen, x, y, scale, angle):
+        self.scaleX = scaleX
+        self.scaleY = scaleY
+        self.bgColor = bgColor
+        self.text = text
+        self.textColor = textColor
+        self.textSize = fontSize
+        self.smooth = smooth
+        self.borderSize = borderSize
+        self.font = pygame.font.Font(None, fontSize)
         self.screen = screen
-        self.x = x
-        self.y = y
-        self.scale = scale
-        self.angle = angle
-        self.points = []
-        self.triangles = []
-        self.center_point = Point(self.screen, self.x, self.y)
-        points_pos = generate_polygon_points(self.angle, self.scale, self.x, self.y)
-        for i in points_pos:
-            self.points.append(Point(self.screen, i[0], i[1]))
-        for i in range(len(points_pos)):
-            self.triangles.append(Triangle(screen, self.points[i-1], self.points[i], self.center_point))
+        if self.text.count(".") == 0:
+            self.textSurfaces = [self.font.render(self.text, True, self.textColor)]
+        else:
+            self.l = self.text.split('.')
+            self.textSurfaces = []
+            for i in range(len(self.l)):
+                self.textSurfaces.append(self.font.render(self.l[i], True, self.textColor))
 
     def draw(self):
-        draw_all(self.triangles)
+        pygame.draw.rect(self.screen, self.bgColor, (self.x, self.y, self.scaleX, self.scaleY),
+                         border_radius=self.smooth)
+        if self.borderSize != -1:
+            for i in range(len(self.textSurfaces)):
+                self.screen.blit(self.textSurfaces[i], (self.x+5+self.borderSize, self.y+5+self.textSize*i+self.borderSize))
+            pygame.draw.rect(self.screen, self.textColor, (self.x, self.y, self.scaleX, self.scaleX), self.borderSize,
+                             border_radius=self.smooth)
+        else:
+            for i in range(len(self.textSurfaces)):
+                self.screen.blit(self.textSurfaces[i], (self.x+5, self.y+5+self.textSize*i))
 
-    def collide(self, x, y):
-        for i in self.triangles:
-            if point_in_triangle((x, y), i):
-                i.color_background = [128, 128, 128]
+    def isClick(self, event, button=1):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == button \
+                    and self.x < x < self.scaleX+self.x \
+                    and self.y < y < self.scaleY+self.y:
+                return True
             else:
-                i.color_background = [255, 255, 255]
-
-    def click(self, x, y, pressed_key, old_pressed_key):
-        for i in self.triangles:
-            if point_in_triangle((x, y), i) and \
-                    ("Key.ctrl_l" in old_pressed_key and "Key.shift" in old_pressed_key and
-                     "Key.alt_l" in old_pressed_key and len(old_pressed_key) == 3) and \
-                    pressed_key == []:
-                print(f"{i.file_name}.py запущен")
-                os.system(f"python scripts/{i.file_name}.py")
-
-
-class Triangle:
-    def __init__(self, screen, point1, point2, point3):
-        self.color_background = [255, 255, 255]
-        self.text = None
-        self.file_name = None
-        self.point1 = point1
-        self.point2 = point2
-        self.point3 = point3
-        self.screen = screen
-        self.lines = [Line(self.screen, point1.x, point2.x, point1.y, point2.y),
-                      Line(self.screen, point2.x, point3.x, point2.y, point3.y),
-                      Line(self.screen, point1.x, point3.x, point1.y, point3.y)]
-
-    def draw(self):
-        font = pygame.font.Font(None, 20)
-        text_width, text_height = font.size(self.file_name)
-        self.text = Text(self.screen, self.file_name, (self.point1.x + self.point2.x + self.point3.x)/3-text_width/2,
-                         (self.point1.y + self.point2.y + self.point3.y) / 3)
-        pygame.draw.polygon(self.screen, self.color_background, ([self.point1.x, self.point1.y],
-                                                     [self.point2.x, self.point2.y],
-                                                     [self.point3.x, self.point3.y]), 0)
-        draw_all(self.lines)
-        self.text.draw()
-
-
-class Line:
-    def __init__(self, screen, x1, x2, y1, y2):
-        self.screen = screen
-        self.pos1 = [x1, y1]
-        self.pos2 = [x2, y2]
-
-    def draw(self):
-        pygame.draw.line(self.screen, [0, 0, 0], self.pos1, self.pos2, 5)
+                return False
 
 
 class Text:
-    def __init__(self, screen, text, x, y, font_size=32, color=(0, 0, 0)):
+    def __init__(self, screen, text, x, y, fontSize=16, textColor=(0, 0, 0)):
         self.screen = screen
-        self.text = text
-        self.font_size = font_size
-        self.color = color
         self.x = x
         self.y = y
-        self.font_name = None
-        self.font = pygame.font.Font(None, self.font_size)
-        self.rendered_text = []
-        self.texts = self.text.split("/")
-        for i in self.texts:
-            self.rendered_text.append(self.font.render(i, True, self.color))
-
-    def draw(self):
-        current_y = self.y
-        for i in self.rendered_text:
-            self.screen.blit(i, (self.x, current_y))
-            current_y += self.font_size/1.5
-
-    def set_text(self, text):
+        self.fontSize = fontSize
         self.text = text
-        self.rendered_text = []
-        self.texts = self.text.split("/")
-        for i in self.texts:
-            self.rendered_text.append(self.font.render(i, True, self.color))
-
-
-class Button:
-    def __init__(self, screen, rect, color):
-        self.x = rect[0]
-        self.y = rect[1]
-        self.scaleX = rect[2]
-        self.scaleY = rect[3]
-        self.screen = screen
-        self.color = color
-        self.rect = rect
-
-    def is_click(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mousepos = pygame.mouse.get_pos()
-                if self.x < mousepos[0] < self.scaleX+self.x:
-                    if self.y < mousepos[1] < self.scaleY+self.y:
-                        return True
-        return False
+        self.font = pygame.font.Font(None, fontSize)
+        self.textColor = textColor
+        self.textSurfaces = self.font.render(self.text, True, self.textColor)
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        self.screen.blit(self.textSurfaces, (self.x, self.y))
 
+    def setText(self, text):
+        self.text = text
+        self.textSurfaces = self.font.render(self.text, True, self.textColor)
--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-import pygame
+import pygame_gui
 
 from classes import *
 
 pygame.init()
-size = [800, 600]
+size = (800, 600)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-text = Text(screen, "Hello, world!", 100, 50)
+texts = []
+input_path_manager = pygame_gui.UIManager(size)
+
+stages = ["input_path_file", '']
+stage_number = 0
+stage = stages[stage_number]
+input_path_text = pygame_gui.elements.UITextEntryLine(
+    relative_rect=pygame.Rect((330, 6), (350, 32)),
+    manager=input_path_manager
+)
+input_path_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((690, 5), (48, 32)),
+    text='Далее',
+    manager=input_path_manager
+)
+
 running = True
 while running:
+    time_delta = clock.tick(60) / 1000.0
+    print(pygame.mouse.get_pos())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if stage == "input_path_file":
+                if event.ui_element == input_path_button:
+                    stage_number += 1
+                    stage = stages[stage_number]
+        input_path_manager.process_events(event)
     screen.fill((255, 255, 255))
-    text.draw()
+    if stage == "input_path_file":
+        Text(screen, "Введите путь к файлу excel:", 10, 10, fontSize=32).draw()
+        input_path_manager.update(time_delta)
+        input_path_manager.draw_ui(screen)
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(60)
 pygame.quit()
